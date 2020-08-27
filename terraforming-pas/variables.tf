b53e7889e878a4f19903f8a281f01fbf5f2c2ff9
duboc <andersonduboc@gmail.com>
2020-08-27 17:24:41 -0300

    updated syntax

--- terraforming-pas/variables.tf
+++ terraforming-pas/variables.tf
@@ -10,10 +10,11 @@ variable "region" {}
 
 variable "availability_zones" {
   type = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "vpc_cidr" {
-  type    = "string"
+  type    = string
   default = "10.0.0.0/16"
 }
 
@@ -53,7 +54,7 @@ variable "create_backup_pas_buckets" {
 *****************/
 
 variable "ops_manager_ami" {
-  type        = "string"
+  type        = string
   description = "Ops Manager AMI on AWS. Ops Manager VM will be skipped if this is empty"
 }
 
@@ -90,25 +91,25 @@ variable "rds_instance_count" {
 ********************/
 
 variable "ssl_cert" {
-  type        = "string"
+  type        = string
   description = "the contents of an SSL certificate to be used by the LB, optional if `ssl_ca_cert` is provided"
   default     = ""
 }
 
 variable "ssl_private_key" {
-  type        = "string"
+  type        = string
   description = "the contents of an SSL private key to be used by the LB, optional if `ssl_ca_cert` is provided"
   default     = ""
 }
 
 variable "ssl_ca_cert" {
-  type        = "string"
+  type        = string
   description = "the contents of a CA public key to be used to sign the generated LB certificate, optional if or `ssl_cert` is provided"
   default     = ""
 }
 
 variable "ssl_ca_private_key" {
-  type        = "string"
+  type        = string
   description = "the contents of a CA private key to be used to sign the generated LB certificate, optional if or `ssl_cert` is provided"
   default     = ""
 }
@@ -118,25 +119,25 @@ variable "ssl_ca_private_key" {
  *****************************/
 
 variable "isoseg_ssl_cert" {
-  type        = "string"
+  type        = string
   description = "the contents of an SSL certificate to be used by the LB, optional if `isoseg_ssl_ca_cert` is provided"
   default     = ""
 }
 
 variable "isoseg_ssl_private_key" {
-  type        = "string"
+  type        = string
   description = "the contents of an SSL private key to be used by the LB, optional if `isoseg_ssl_ca_cert` is provided"
   default     = ""
 }
 
 variable "isoseg_ssl_ca_cert" {
-  type        = "string"
+  type        = string
   description = "the contents of a CA public key to be used to sign the generated iso seg LB certificate, optional if `isoseg_ssl_cert` is provided"
   default     = ""
 }
 
 variable "isoseg_ssl_ca_private_key" {
-  type        = "string"
+  type        = string
   description = "the contents of a CA private key to be used to sign the generated iso seg LB certificate, optional if `isoseg_ssl_cert` is provided"
   default     = ""
 }
@@ -146,7 +147,7 @@ variable "isoseg_ssl_ca_private_key" {
 *********/
 
 variable "tags" {
-  type        = "map"
+  type        = map
   default     = {}
   description = "Key/value tags to assign to all AWS resources"
 }
@@ -156,7 +157,7 @@ variable "tags" {
  *******************************/
 
 variable "create_isoseg_resources" {
-  type        = "string"
+  type        = string
   default     = "0"
   description = "Optionally create a LB and DNS entries for a single isolation segment. Valid values are 0 or 1."
 }
